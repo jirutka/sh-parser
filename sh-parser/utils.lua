743a1b3995d4286ddb101dfdcd5fc141b15eee9b
Jakub Jirutka <jakub@jirutka.cz>
2017-01-14 13:43:22 +0100

    Fix code-style in utils

--- sh-parser/utils.lua
+++ sh-parser/utils.lua
@@ -43,9 +43,10 @@ M.LUA_V = _VERSION:sub(-3)
 -- @param value The value to assert.
 -- @tparam string ttype The expected type.
 -- @raise if the argument n is not the correct type.
-function M.assert_arg(n, value, ttype)
+function M.assert_arg (n, value, ttype)
   if type(value) ~= ttype then
-    error(("bad argument #%d: expected a '%s', got a '%s'"):format(n, ttype, type(value)), 2)
+    error(("bad argument #%d: expected a '%s', got a '%s'")
+          :format(n, ttype, type(value)), 2)
   end
 end
 
