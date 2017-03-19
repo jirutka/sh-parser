bf41b513c10facc8ff46a88233721aa37e9fe91d
Jakub Jirutka <jakub@jirutka.cz>
2017-03-20 00:00:56 +0100

    Use location_resolver in ast.simple, add new option loc2

--- sh-parser/ast/simple.lua
+++ sh-parser/ast/simple.lua
@@ -3,9 +3,15 @@
 --
 -- Builds simple AST suitable for encoding into JSON.
 ----
-local name_captures  = require 'sh-parser.ast.name_captures'
+local loc_resolver  = require 'sh-parser.location_resolver'
+local name_captures = require 'sh-parser.ast.name_captures'
 
 local push = table.insert
+
+
+local function named_loc (row, col, pos)
+  return { row = row, col = col, pos = pos }
+end
 
 
 --- Creates a function that transforms positional captures produced by the
@@ -16,6 +22,8 @@ local push = table.insert
 -- * `comments`: Include comments.
 -- * `loc`:      Include nodes' location as start and end absolute position in
 --               the `subject`.
+-- * `loc2`:     Include nodes' location as start and end row, column and
+--               absolute position in the `subject`.
 -- * `source`:   Include raw source in all nodes except the root node.
 --
 -- @function __call
@@ -26,7 +34,13 @@ return function (subject, opts)
   opts = opts or {}
   local comments = opts.comments and {} or nil
   local with_loc = opts.loc or false
+  local with_loc2 = opts.loc2 or false
   local with_source = opts.source or false
+
+  local resolve_loc
+  if with_loc2 then
+    resolve_loc = loc_resolver(subject)
+  end
 
   --- Creates a new AST node.
   --
@@ -40,7 +54,12 @@ return function (subject, opts)
     local node = name_captures(node_type, captures)
     node.type = node_type
 
-    if with_loc then
+    if with_loc2 then
+      node.loc = {
+        ['start'] = named_loc(resolve_loc(start_pos)),
+        ['end'] = named_loc(resolve_loc(end_pos)),
+      }
+    elseif with_loc then
       node.loc = {
         ['start'] = start_pos,
         ['end'] = end_pos,
